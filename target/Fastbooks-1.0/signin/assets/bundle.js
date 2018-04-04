de91781f2a7e6ddb56894a75d54100c91e87f272
DELL <DELL@Laptop>
2018-04-04 11:04:55 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/com/fastbooks/modelo/FbCompania.java
    	src/main/java/com/fastbooks/modelo/FbDireccion.java
    	src/main/resources/com/fastbooks/messages_en.properties
    	src/main/webapp/template/template.xhtml

--- target/Fastbooks-1.0/signin/assets/bundle.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
-    $(".mm-toggle").click(function(e) {
-        e.preventDefault();
-        $(".mm-container").toggleClass("active");
-        $("body").toggleClass("menu-open");
-    });
-
-});
